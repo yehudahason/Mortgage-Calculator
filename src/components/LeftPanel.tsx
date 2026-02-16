15f41a0f3703fdd3107f012f989395952a93af50
Yehuda Hason <yehudaxt@gmail.com>
2026-02-16 11:53:59 +0200

    input

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -10,35 +10,38 @@ const LeftPanel = () => {
 
       <form>
         {/* Mortgage Amount*/}
-        <div className="form-group">
+        <div className="form-group err">
           <label htmlFor="amount">Mortgage Amount</label>
           <div className="input-group">
-            <span>£</span>
+            <span className="left">£</span>
+            <span className="err amount">This field is required</span>
             <input type="text" id="amount" />
           </div>
         </div>
 
         {/*<!-- Term & Rate -->*/}
         <div className="form-row">
-          <div className="form-group">
+          <div className="form-group err">
             <label htmlFor="term">Mortgage Term</label>
             <div className="input-group">
               <input type="text" id="term" style={{ paddingLeft: "18px" }} />
-              <span>years</span>
+              <span className="right">years</span>
+              <span className="err years">This field is required</span>
             </div>
           </div>
 
-          <div className="form-group">
+          <div className="form-group err">
             <label htmlFor="rate">Interest Rate</label>
             <div className="input-group">
               <input type="text" id="rate" style={{ paddingLeft: "18px" }} />
-              <span>%</span>
+              <span className="right">%</span>
+              <span className="err rate">This field is required</span>
             </div>
           </div>
         </div>
 
         {/* Mortgage Type */}
-        <div className="form-group">
+        <div className="form-group err">
           <label>Mortgage Type</label>
           <div className="radio-group">
             <label>
@@ -49,6 +52,7 @@ const LeftPanel = () => {
               <input type="radio" name="type" checked />
               Interest Only
             </label>
+            <span className="err radio">This field is required</span>
           </div>
         </div>
 
